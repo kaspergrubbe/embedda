ffd6b1b576848a1103228d8351e0d7c509817cd8
Takle <christian.takle@gmail.com>
2013-04-10 01:13:36 +0200

    Adds soundcloud regex to embedda

--- lib/embedda.rb
+++ lib/embedda.rb
@@ -1,14 +1,17 @@
+require 'cgi'
+
 class String
   def embedda(options = {})
-    options = {:filters => [:youtube, :vimeo]}.merge(options)
+    options = {:filters => [:youtube, :vimeo, :soundcloud]}.merge(options)
 
     # Make sure that filters is an array because we allow the short form of
     # "hello".embedda(:filters => :youtube) instead of "hello".embedda(:filters => [:youtube])
     options[:filters] = Array(options[:filters])
 
     compiled = self
-    compiled = youtube_replace(compiled) if options[:filters].include?(:youtube)
-    compiled = vimeo_replace(compiled)   if options[:filters].include?(:vimeo)
+    compiled = youtube_replace(compiled)    if options[:filters].include?(:youtube)
+    compiled = vimeo_replace(compiled)      if options[:filters].include?(:vimeo)
+    compiled = soundcloud_replace(compiled) if options[:filters].include?(:soundcloud)
 
     return compiled
   end
@@ -32,5 +35,19 @@ class String
   end
   def vimeo_player(token)
     "<iframe src=\"http://player.vimeo.com/video/#{token}\" width=\"500\" height=\"281\" frameborder=\"0\" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>"
+  end
+
+  def soundcloud_replace(compiled)
+    r = /(https?:\/\/(?:www.)?soundcloud.com\/[A-Za-z0-9]+(?:[-_][A-Za-z0-9]+)*(?!\/sets(?:\/|$))(?:\/[A-Za-z0-9]+(?:[-_][A-Za-z0-9]+)*){1,2}\/?)/i
+    compiled.gsub!(r) { |match| soundcloud_player(match) }
+
+    return compiled
+  end
+
+  def soundcloud_player(token)
+    url_encoded_string = CGI::escape(token)
+
+    # Note: added '&' ...?url=...&
+    "<iframe width=\"100%\" height=\"166\" scrolling=\"no\" frameborder=\"no\" src=\"https://w.soundcloud.com/player/?url=#{url_encoded_string}&color=ff6600&amp;auto_play=false&amp;show_artwork=false\"></iframe>"
   end
 end
