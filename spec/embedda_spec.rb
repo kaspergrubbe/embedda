48b8a06a3d1d91818df5b40a69538bc371ca3781
Takle <christian.takle@gmail.com>
2013-05-06 22:23:52 +0200

    Updates Specs

--- spec/embedda_spec.rb
+++ spec/embedda_spec.rb
@@ -87,33 +87,6 @@ describe Embedda do
     end
   end
 
-  context "Soundcloud-link" do
-    let(:embed_string) { '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A%2F%2Fsoundcloud.com%2Fflume-1%2Fflume-left-alone-bobby-tank&color=ff6600&amp;auto_play=false&amp;show_artwork=false"></iframe>' }
-    let(:link)         { 'https://soundcloud.com/flume-1/flume-left-alone-bobby-tank' }
-
-    it "should embed when text have a link" do
-      @story = link
-      expect(embedda).to eq(embed_string)
-    end
-
-    it "should embed when also other text is present around link" do
-      @story = "Hello, my name is Takle: #{link}<br/>And I am embedding links"
-      expect(embedda).to eq("Hello, my name is Takle: #{embed_string}<br/>And I am embedding links")
-    end
-
-    it "should embed when enabled" do
-      @story  = "Hello, my name is Takle: #{link}<br/>And I am embedding links"
-      embedda = described_class.new(@story, :filters => :soundcloud).embed
-      expect(embedda).to eq("Hello, my name is Takle: #{embed_string}<br/>And I am embedding links")
-    end
-
-    it "should not embed when disabled" do
-      @story  = "Hello, my name is Takle: #{link}<br/>And I am not embedding links"
-      embedda = described_class.new(@story, :filters => :youtube).embed
-      expect(embedda).to eq("Hello, my name is Takle: #{link}<br/>And I am not embedding links")
-    end
-  end
-
   context "All embeds in one string" do
       let(:youtube)         { '<iframe width="560" height="315" src="http://www.youtube.com/embed/dQw4w9WgXcQ" frameborder="0" allowfullscreen></iframe>' }
       let(:youtube_link)    { 'http://www.youtube.com/watch?v=dQw4w9WgXcQ' }
@@ -149,5 +122,4 @@ describe Embedda do
       expect { described_class.new(story, :filters => [:dummy]).embed }.to raise_error(Embedda::UnknownFilter)
     end
   end
-
 end
