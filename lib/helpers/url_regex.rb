fcf4db8e4ed5c8f772abec56de3672656e049de8
Takle <christian.takle@gmail.com>
2013-05-06 22:50:59 +0200

    Trys to pass 1.8.7 and ree on travis

--- lib/helpers/url_regex.rb
+++ lib/helpers/url_regex.rb
@@ -4,9 +4,12 @@ module Regex
   #     ...http://...
   #     ...https://...
   #   where ... is arbitrary text
-  URL = { :scheme => / # Match a Uniform Resource Locator scheme
-                       (?:http|https) # http or https       non-capturing group
-                       :\/\/          # colon-slash-slash
-                       (?:www\.)?     # Optional www.       non-capturing group
-                     /x }
+  # URL = { :scheme => / # Match a Uniform Resource Locator scheme
+  #                      (?:http|https) # http or https       non-capturing group
+  #                      :\/\/          # colon-slash-slash
+  #                      (?:www\.)?     # Optional www.       non-capturing group
+  #                    /x }
+
+  # 1.8.7 and ree doesn't support the above syntax
+  URL = { :scheme => /(?:http|https):\/\/(?:www\.)?/ }
 end
