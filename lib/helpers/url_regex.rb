4474438f4cd7a4fdeeafa56013ab46bf9a84fdd1
Takle <christian.takle@gmail.com>
2013-05-14 00:11:30 +0200

    Returns multiline regex

--- lib/helpers/url_regex.rb
+++ lib/helpers/url_regex.rb
@@ -4,12 +4,9 @@ module Regex
   #     ...http://...
   #     ...https://...
   #   where ... is arbitrary text
-  # URL = { :scheme => / # Match a Uniform Resource Locator scheme
-  #                      (?:http|https) # http or https       non-capturing group
-  #                      :\/\/          # colon-slash-slash
-  #                      (?:www\.)?     # Optional www.       non-capturing group
-  #                    /x }
-
-  # 1.8.7 and ree doesn't support the above syntax
-  URL = { :scheme => /(?:http|https):\/\/(?:www\.)?/ }
+  URL = { :scheme => / # Match a Uniform Resource Locator scheme
+                       (?:http|https) # http or https       non-capturing group
+                       :\/\/          # colon-slash-slash
+                       (?:www\.)?     # Optional www.       non-capturing group
+                     /x }
 end
